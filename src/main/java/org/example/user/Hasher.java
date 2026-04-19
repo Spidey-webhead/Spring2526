cf7c84a82f393dcf336e4ce22290682346ca9c18
Spidey-webhead <kowalskanatalia970@gmail.com>
2026-04-19 13:32:50 +0200

    zad5

--- src/main/java/org/example/user/Hasher.java
+++
@@ -1,9 +0,0 @@
-package org.example.user;
-
-import org.apache.commons.codec.digest.DigestUtils;
-
-public class Hasher {
-    public static String hashPassword(String password) {
-        return DigestUtils.sha256Hex(password);
-    }
-}
